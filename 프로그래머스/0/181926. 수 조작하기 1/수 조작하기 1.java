e4a92a77d22695773119e51ddab41ef39124baf5
강성현 <sambean0911@gmail.com>
2025-12-18 14:45:34 +0900

    [level 0] Title: 수 조작하기 1, Time: 7.93 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,23 +1,22 @@
 class Solution {
     public int solution(int n, String control) {
-        int answer = n;
+        int answer = 0;
+        answer = n ;
+        char a;
         for(int i = 0; i < control.length(); i++){
-            char a  = control.charAt(i);
-            switch(a){
-                case 'w': 
-                    answer += 1;
-                    break;
-                case 's':
-                    answer -= 1;
-                    break;
-                case 'd': 
-                    answer += 10;
-                    break;
-                case 'a':
-                    answer -= 10;
-                    break;
+            a = control.charAt(i);
+            if(a == 'w'){
+                answer += 1;    
             }
-                                                
+            else if(a== 's'){
+                answer -= 1;
+            }
+            else if(a== 'd'){
+                answer += 10;
+            }
+            else{
+                answer -= 10;
+            }
         }
         return answer;
     }
