ca479ceb86bc73d31886cadea4f6f7e7197cbf0d
강성현 <sambean0911@gmail.com>
2025-12-28 06:52:03 +0900

    [level 0] Title: 배열 만들기 2, Time: 6.36 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.ArrayList;
+class Solution {
+    public int[] solution(int l, int r) {
+        int[] answer = {};
+        int[] five = new int[65];
+        for(int i = 0; i < 65; i++){
+            five[i] = Integer.valueOf((Integer.toBinaryString(i+1)).replaceAll("1", "5"));
+        }
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < 65; i++){
+            if(five[i] >= l && five[i] <= r){
+                list.add(five[i]);
+            }
+        }
+        if(list.size() == 0 ){
+            list.add(-1);
+        }
+        answer = list.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
+        return answer;
+    }
+}
