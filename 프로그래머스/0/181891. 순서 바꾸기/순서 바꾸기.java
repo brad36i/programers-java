c716b2eb9fd87b066ef72bd224ac7186581d2f33
강성현 <sambean0911@gmail.com>
2025-12-31 05:53:07 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 90 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+        int[] front = Arrays.copyOfRange(num_list, 0, n);
+        int[] back = Arrays.copyOfRange(num_list, n, num_list.length);
+        answer = new int[num_list.length];
+        System.arraycopy(back, 0 , answer, 0, back.length);
+        System.arraycopy(front, 0, answer, back.length, front.length);
+        return answer;
+    }
+}
