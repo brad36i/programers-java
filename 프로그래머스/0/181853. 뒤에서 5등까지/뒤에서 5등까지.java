a5b65f4b389769ce6da3bfd99c96eb9a3a817ec9
강성현 <sambean0911@gmail.com>
2026-01-05 06:32:17 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 1.30 ms, Memory: 90 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+        Arrays.sort(num_list);
+        answer = new int[5];
+        for(int i = 0; i < 5; i++){
+            answer[i] = num_list[i];
+        }
+        return answer;
+    }
+}
