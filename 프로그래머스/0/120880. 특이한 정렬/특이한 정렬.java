a6801e93b3bdbab22983e6ba8170e6e64af09bca
강성현 <sambean0911@gmail.com>
2026-01-14 07:13:21 +0900

    [level 0] Title: 특이한 정렬, Time: 0.84 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        int[] answer = {};
+        
+        int tmp = 0;
+        int length = numlist.length;
+        for(int i = 0; i < length; i++){
+            for(int j = 0; j < length; j++){
+                if(Math.abs(n - numlist[i]) < Math.abs(n - numlist[j])){
+                    tmp = numlist[i];
+                    numlist[i] = numlist[j];
+                    numlist[j] = tmp;
+                }
+                else if(Math.abs(n - numlist[i]) == Math.abs(n - numlist[j])){
+                    if(numlist[i] < numlist[j]){
+                        
+                    }
+                    else{
+                        tmp = numlist[i];
+                        numlist[i] = numlist[j];
+                        numlist[j] = tmp;
+                    }
+                }
+            }
+        }
+        answer = numlist;
+        return answer;
+    }
+}
