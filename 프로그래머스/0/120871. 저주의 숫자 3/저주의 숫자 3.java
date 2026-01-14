ecb7272244bbc0f3633fe73d92a43d271e64a6b2
강성현 <sambean0911@gmail.com>
2026-01-15 07:06:02 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.18 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -4,14 +4,13 @@ class Solution {
         int[] curse = new int[100];
         int num = 1;
         int index = 0;
-        while(index < 100){
+        while(index != n){
             if(num % 3 != 0 && String.valueOf(num).contains("3") != true){
-                curse[index] = num;
+                answer = num;
                 index++;
             }
             num++;
         }
-        answer = curse[n - 1];
         return answer;
     }
 }
