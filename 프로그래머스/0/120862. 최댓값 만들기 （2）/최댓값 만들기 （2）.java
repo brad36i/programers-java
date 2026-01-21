320b9eeb7d62506274c5725505ba5d330ecb7798
강성현 <sambean0911@gmail.com>
2026-01-22 05:23:26 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.12 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,13 +1,6 @@
 class Solution {
     public int solution(int[] numbers) {
-        int answer = numbers[0] * numbers[1];
-        for(int i = 0; i < numbers.length; i++){
-            for(int j = i+1;j < numbers.length; j++){
-                int sum = numbers[i] * numbers[j];
-                if(sum > answer)
-                    answer = sum;
-            }
-        }
+        int answer = 0;
         return answer;
     }
 }
